bac667c8083af60ff0ef8b020ec9564cfcf3bedf
Sam Boylett <sam.boylett@netsells.co.uk>
2019-03-07 16:45:37 +0000

    Build component

--- src/vue-form-state.vue
+++ src/vue-form-state.vue
@@ -1,5 +1,45 @@
 <template>
     <div class="vue-form-state">
-        Hello world
+        <slot
+            :loading="loading"
+            :error="error"
+            :submit="handleSubmit"
+        />
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'vue-form-state',
+
+        props: {
+            submit: {
+                type: Function,
+                required: true,
+            },
+        },
+
+        data() {
+            return {
+                loading: false,
+                error: null,
+                result: null,
+            };
+        },
+
+        methods: {
+            async handleSubmit(...args) {
+                try {
+                    this.loading = true;
+                    this.error = null;
+
+                    this.result = await this.submit(...args);
+                } catch(e) {
+                    this.error = e;
+                } finally {
+                    this.loading = false;
+                }
+            },
+        },
+    };
+</script>
